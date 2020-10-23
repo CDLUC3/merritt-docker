80c559a994b906b2559e9a77b722a3efee356dd3
Terry Brady <terrywbrady@gmail.com>
2020-10-23 15:22:33 -0700

    mock service

--- mrt-services/ezid_mock/app.rb
+++ mrt-services/ezid_mock/app.rb
@@ -3,20 +3,46 @@ require 'sinatra/base'
 
 set :bind, '0.0.0.0'
 
-get '/*' do
-  puts("GET #{params}")
-  status 200
-  "GET"
+def fake_ark(shoulder)
+  # force a relatively unique 7 digit number
+  num = Time.now.to_i % 8999999 + 1000000
+  "#{shoulder}#{num}"
 end
 
-post '/*' do
-  puts("POST #{params}")
+# get method is just for debugging
+get '/id/*' do
+  ark = params['splat'][0]
   status 200
-  "POST"
+  "success: #{ark}"
+end
+
+# get method is just for debugging
+get '/shoulder/*' do
+  shoulder = params['splat'][0]
+  ark = fake_ark(shoulder)
+  status 200
+  "success: #{ark}"
+end
+
+get '/*' do
+  status 200
+  "N/A"
 end
 
 put '/*' do
-  puts("PUT #{params}")
   status 200
-  "PUT"
+  "N/A"
+end
+
+post '/id/*' do
+  ark = params['splat'][0]
+  status 200
+  "success: #{ark}"
+end
+
+post '/shoulder/*' do
+  shoulder = params['splat'][0]
+  ark = fake_ark(shoulder)
+  status 200
+  "success: #{ark}"
 end

842bcc3d7df42d6e10987292e84405a684937b05
Terry Brady <tbrady@uc3-mrtdocker01x2-dev.cdlib.org>
2020-11-25 08:55:25 -0800

    add trigger

--- mrt-services/mysql/init.sql
+++ mrt-services/mysql/init.sql
@@ -318,6 +318,47 @@ ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 
 flush privileges;
 
+# Triggers
+
+DELIMITER //
+DROP TRIGGER IF EXISTS insert_fulltext//
+CREATE TRIGGER insert_fulltext
+AFTER INSERT ON inv_dublinkernels
+FOR EACH ROW
+BEGIN
+  IF NOT NEW.value='(:unas)' THEN
+    INSERT INTO sha_dublinkernels
+    VALUES (NEW.id, NEW.value);
+  END IF;
+END;
+//
+DROP TRIGGER IF EXISTS update_fulltext//
+CREATE TRIGGER update_fulltext
+AFTER UPDATE ON inv_dublinkernels
+FOR EACH ROW
+BEGIN
+  IF NEW.value!='(:unas)' THEN
+    UPDATE sha_dublinkernels
+    SET value = NEW.value
+    WHERE id = NEW.id;
+  END IF;
+END;
+//
+DROP TRIGGER IF EXISTS delete_fulltext//
+CREATE TRIGGER delete_fulltext
+AFTER DELETE ON inv_dublinkernels
+FOR EACH ROW
+BEGIN
+  DELETE FROM sha_dublinkernels
+  WHERE id = OLD.id;
+END;
+//
+DELIMITER ;
+insert into sha_dublinkernels 
+select id, value from inv_dublinkernels 
+where not inv_dublinkernels.element='where' 
+and not inv_dublinkernels.value='(:unas)';
+
 # Configure content
 
 insert into inv_collections(ark,name,mnemonic,harvest_privilege)

fdb02637577ea5fb00636ada4804224595d9976b
Terry Brady <tbrady@uc3-mrtdocker01x2-dev.cdlib.org>
2020-12-02 16:53:31 -0800

    add integ tests

--- workspace-init/merritt-docker-dev-init.sh
+++ workspace-init/merritt-docker-dev-init.sh
@@ -6,6 +6,7 @@ rm -rf merritt-docker merritt-docker-prv
 
 git clone git@github.com:CDLUC3/merritt-docker.git
 git clone git@github.com:cdlib/merritt-docker-prv.git
+git clone git@github.com:cdlib/mrt-integ-tests.git
 
 mkdir merritt-docker/mrt-services/no-track
 cp -r merritt-docker-prv/* merritt-docker/mrt-services/no-track

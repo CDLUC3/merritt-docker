8c9d83fe63c29f4f7d25279a688507cc6b9ea3ac
Terry Brady <terrence.brady@ucop.edu>
2021-08-12 10:47:55 -0700

    tidy docker dependencies

--- mrt-services/ingest/admin-submit/sample_owner.sh
+++
@@ -1,7 +0,0 @@
-curl -v \
--F "file=@/tdr/ingest/admin-submit/sample_owner.checkm" \
--F "type=single-file-batch-manifest" \
--F "submitter=merritt-test" \
--F "responseForm=xml" \
--F "profile=admin/docker/owner/sample_owner" \
-http://ingest:8080/ingest/poster/update
--- mrt-services/ingest/admin-submit/sample_service_level_agreement.sh
+++
@@ -1,7 +0,0 @@
-curl -v \
--F "file=@/tdr/ingest/admin-submit/sample_service_level_agreement.checkm" \
--F "type=single-file-batch-manifest" \
--F "submitter=merritt-test" \
--F "responseForm=xml" \
--F "profile=admin/docker/sla/sample_service_level_agreement" \
-http://ingest:8080/ingest/poster/update

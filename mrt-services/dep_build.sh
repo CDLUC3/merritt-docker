6e644c7ebfb565ca24df3de317946d0ff4206f05
Terry Brady <terrence.brady@ucop.edu>
2022-01-05 12:12:29 -0800

    build core services w/o oai and sword

--- mrt-services/dep_build.sh
+++ mrt-services/dep_build.sh
@@ -10,10 +10,6 @@ docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-cor
 
 docker push ${ECR_REGISTRY}/mrt-core2:dev
 
-docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-cloud:dev dep_cloud
-
-docker push ${ECR_REGISTRY}/mrt-cloud:dev
-
 docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/cdl-zk-queue:dev dep_cdlzk
 
 docker push ${ECR_REGISTRY}/cdl-zk-queue:dev
@@ -22,6 +18,30 @@ docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-zoo
 
 docker push ${ECR_REGISTRY}/mrt-zoo:dev
 
+docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-cloud:dev dep_cloud
+
+docker push ${ECR_REGISTRY}/mrt-cloud:dev
+
 docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-ingest:dev ingest
 
 docker push ${ECR_REGISTRY}/mrt-ingest:dev
+
+docker build -f inventory/Dockerfile-jar --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-inventory-src:dev inventory
+
+docker push ${ECR_REGISTRY}/mrt-inventory-src:dev
+
+docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-inventory:dev inventory
+
+docker push ${ECR_REGISTRY}/mrt-inventory:dev
+
+docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-store:dev store
+
+docker push ${ECR_REGISTRY}/mrt-store:dev
+
+docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-audit:dev audit
+
+docker push ${ECR_REGISTRY}/mrt-audit:dev
+
+docker build --build-arg ECR_REGISTRY=${ECR_REGISTRY} -t ${ECR_REGISTRY}/mrt-replic:dev replic
+
+docker push ${ECR_REGISTRY}/mrt-replic:dev

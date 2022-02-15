37f379139603e298ddbb88d36a0ef9d66a82c031
Ashley Gould <agould@ucop.edu>
2022-02-14 17:57:39 -0800

    java-refactore: tweeking workspace init script

--- bin/initialize_workspace.sh
+++ bin/initialize_workspace.sh
@@ -3,17 +3,20 @@
 
 START_DIR=$(pwd)
 SCRIPT_HOME=$(dirname $0)
-WORKSPACE_DIR="$SCRIPT_HOME/.."
+WORKSPACE_DIR=$(realpath $SCRIPT_HOME/..)
+PARENT_DIR=$(realpath $WORKSPACE_DIR/..)
 #WORKSPACE_BRANCH="java-refactor"
 WORKSPACE_BRANCH="java-refactor.ashley"
-PARENT_DIR="$WORKSPACE_DIR/.."
 TESTS_DIR="$PARENT_DIR/mrt-integ-tests"
 TESTS_REPO="git@github.com:cdluc3/mrt-integ-tests.git"
 
 # Ensure workspace is clean
+echo "Initializing Merritt Workpace: $WORKSPACE_DIR"
+echo "Repository branch: $WORKSPACE_BRANCH"
 cd $WORKSPACE_DIR
 git checkout -q $WORKSPACE_BRANCH
-if [ -z "$(git status --porcelain)" ]; then
+if [ -z "$(git status --porcelain --ignore-submodules)" ]; then
+  echo
   echo "Working directory clean"
 else
   echo
@@ -26,11 +29,12 @@ fi
 echo
 echo "Updating submodules..."
 git submodule update --remote --recursive --init -- .
-exit 0
+git submodule status
 
+echo
 echo "cloning mrt-integ-tests repo"
+[ -d $TESTS_DIR ] && rm -rf $TESTS_DIR
 cd $PARENT_DIR
-[-d $TESTS_DIR ] && rm -rf $TESTS_DIR
 git clone $TESTS_REPO
 
 

e28eeac44d14a9fb9f1cae3cf223bf80a4cf9402
Terry Brady <terrence.brady@ucop.edu>
2022-07-25 16:24:24 -0700

    init storage nodes

--- mrt-inttest-services/mock-merritt-it/app.rb
+++ mrt-inttest-services/mock-merritt-it/app.rb
@@ -1,22 +1,25 @@
 require 'sinatra'
 require 'sinatra/base'
+require 'cgi'
 
 set :bind, '0.0.0.0'
-
-get '/store/state/7777?t=anvl' do
-  send_file "/data/7777.anvl"
-end
 
 get '/store/state/7777' do
   send_file "/data/7777.anvl" if params[:t] == "anvl"
   "success: store/state/7777"
 end
 
-get '/store/state/8888?t=anvl' do
-  send_file "/data/8888.anvl"
+get '/store/state/8888' do
+  send_file "/data/8888.anvl" if params[:t] == "anvl"
+  "success: store/state/8888"
 end
 
 get '/static/*' do
   val = params['splat'][0]
-  send_file "/data/static/#{val}"
+  fname = "/data/static/#{val}"
+  if File.exist?(fname)
+    send_file "/data/static/#{val}"
+  else
+    "Not found: #{fname}"
+  end
 end

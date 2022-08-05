b8b7bc23152018b618fe89bbe112a32d179f748f
Terry Brady <terrence.brady@ucop.edu>
2022-08-05 09:29:51 -0700

    create sql

--- mrt-inttest-services/mock-merritt-it/create_audit_replic_data.sh
+++ mrt-inttest-services/mock-merritt-it/create_audit_replic_data.sh
@@ -19,6 +19,23 @@ curl -v -X POST http://it-server:8080/mrtinv/add -F responseForm=json \
 curl -v -X POST http://it-server:8080/mrtinv/add -F responseForm=json \
      -F url=http://mock-merritt-it:4567/storage/manifest/7777/ark%3A%2F1111%2F4444
 
+echo "*** Add record to inv_collections_inv_nodes"
+
+# Run SQL to add a record to inv_collections_inv_nodes
+mysql -h mrt-it-database -u user --password=password --database=inv << EOF
+insert 
+  into inv_collections_inv_nodes(
+    inv_collection_id, 
+    inv_node_id
+  )
+  select
+    (select id from inv_collections where ark='ark:/99999/collection'),
+    (select id from inv_nodes where number=8888)
+  ;
+EOF
+
+echo "*** Generate MySql dump to be used for audit/replic testing"
+
 echo "/*See https://github.com/CDLUC3/merritt-docker/blob/main/mrt-inttest-services/mock-merritt-it/README.md*/" > //audit_replic_data/audit_replic_data.sql
 echo "/*See https://github.com/CDLUC3/mrt-inventory/blob/main/inv-it/src/test/README.md*/" >> //audit_replic_data/audit_replic_data.sql
 mysqldump -h mrt-it-database -u user --password=password --compact --no-create-info inv >> //audit_replic_data/audit_replic_data.sql

9f5fba58722a17cb1480ee32f91f1809f865c909
Terry Brady <terrence.brady@ucop.edu>
2022-08-26 13:45:22 -0700

    update mock service

--- mrt-inttest-services/mock-merritt-it/app.rb
+++ mrt-inttest-services/mock-merritt-it/app.rb
@@ -3,8 +3,11 @@ require 'sinatra/base'
 require 'mustache'
 require 'redcarpet'
 require 'cgi'
+require 'json'
 
 set :bind, '0.0.0.0'
+
+# Useful functions
 
 def readme
   renderer = Redcarpet::Render::HTML.new
@@ -18,14 +21,6 @@ end
 
 get '' do
   readme
-end
-
-get '/storage/manifest/*/*' do
-  node = params['splat'][0]
-  ark = params['splat'][1]
-  content_type 'application/xml'
-  manifest = ark =~ %r[ark:\/v3.*] ? "manifestv3" : "manifest"
-  Mustache.render(File.open("/data/#{manifest}").read, {'node': node, 'ark': ark})
 end
 
 def get_file(fname)
@@ -51,25 +46,58 @@ def get_producer(params)
   get_file("/data/producer/#{path}")
 end
 
-get '/storage/content/*/*/*/producer/*' do
-  get_producer(params)
-end
-
-get '/store/content/*/*/*/producer/*' do
-  get_producer(params)
-end
-
-get '/storage-input/*' do
-  path = params['splat'][0]
-  get_file("/data/#{path}")
-end
-
 def get_system(params) 
   node = params['splat'][0]
   ark = params['splat'][1]
   ver = params['splat'][2]
   path = params['splat'][3]
   get_file("/data/system/#{path}")
+end
+
+get '/static/*' do
+  fn = params['splat'][0]
+  fname = get_fname(fn)
+  if fname.nil?
+    status 404
+    "Not found: #{fn}"
+  else
+    send_file fname
+  end
+end
+
+def get_fname(val) 
+  fname = "/data/static/#{val}"
+  return fname if File.exist?(fname)
+  fname = fname.gsub(%r[\/0\/], '/1/')
+  return fname if File.exist?(fname)
+  nil
+end
+
+# mock data creation
+
+get '/storage-input/*' do
+  path = params['splat'][0]
+  get_file("/data/#{path}")
+end
+
+
+# Mock Storage
+
+get '/storage/manifest/*/*' do
+  node = params['splat'][0]
+  ark = params['splat'][1]
+  content_type 'application/xml'
+  manifest = ark =~ %r[ark:\/v3.*] ? "manifestv3" : "manifest"
+  Mustache.render(File.open("/data/#{manifest}").read, {'node': node, 'ark': ark})
+end
+
+
+get '/storage/content/*/*/*/producer/*' do
+  get_producer(params)
+end
+
+get '/store/content/*/*/*/producer/*' do
+  get_producer(params)
 end
 
 get '/storage/content/*/*/*/system/*' do
@@ -90,21 +118,47 @@ get '/store/state/8888' do
   "Specify ?t=anvl to download the file"
 end
 
-get '/static/*' do
-  fn = params['splat'][0]
-  fname = get_fname(fn)
-  if fname.nil?
-    status 404
-    "Not found: #{fn}"
-  else
-    send_file fname
-  end
+post '/add/*' do
+  status 200
+  "<entity>foo</entity>"
 end
 
-def get_fname(val) 
-  fname = "/data/static/#{val}"
-  return fname if File.exist?(fname)
-  fname = fname.gsub(%r[\/0\/], '/1/')
-  return fname if File.exist?(fname)
-  nil
+post '/update/*' do
+  status 200
+  "<entity>foo</entity>"
+end
+
+# Mock Inventory
+
+get '/inventory/*' do
+  status 200
+  content_type 'application/xml'
+  "<entity>foo</entity>"
+end
+
+post '/inventory/*' do
+  status 200
+  content_type 'application/xml'
+  "<entity>foo</entity>"
+end
+
+# Mock EZID
+
+def fake_ark(shoulder)
+  # force a relatively unique 7 digit number
+  num = Time.now.to_i % 8999999 + 1000000
+  "#{shoulder}#{num}"
+end
+
+post '/id/*' do
+  ark = params['splat'][0]
+  status 200
+  "success: #{ark}"
+end
+
+post '/shoulder/*' do
+  shoulder = params['splat'][0]
+  ark = fake_ark(shoulder)
+  status 200
+  "success: #{ark}"
 end

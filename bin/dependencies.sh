813a7207deaab449a3c29e0b08aa7ee3505c7202
Terry Brady <terrywbrady@gmail.com>
2023-01-06 17:38:19 -0800

    services pom

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -7,60 +7,7 @@ SCRIPT_HOME=$(dirname $0)
 
 # cd into mrt-services
 REPOS_DIR=${START_DIR}/$SCRIPT_HOME/../mrt-services
-echo "" > ${START_DIR}/dependencies.txt
 
-echo "Analyzing mrt-core"
 cd $REPOS_DIR
-cd dep_core/mrt-core2
 mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing mrt-cloud"
-cd $REPOS_DIR
-cd dep_cloud/mrt-cloud
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing cdl-zk-queue"
-cd $REPOS_DIR
-cd dep_cdlzk/cdl-zk-queue
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing mrt-zoo"
-cd $REPOS_DIR
-cd dep_zoo/mrt-zoo
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing mrt-inventory"
-cd $REPOS_DIR
-cd inventory/mrt-inventory
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing mrt-replic"
-cd $REPOS_DIR
-cd replic/mrt-replic
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing mrt-audit"
-cd $REPOS_DIR
-cd audit/mrt-audit
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing mrt-store"
-cd $REPOS_DIR
-cd store/mrt-store
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo "Analyzing mrt-ingest"
-cd $REPOS_DIR
-cd ingest/mrt-ingest
-mvn clean dependency:analyze
-mvn dependency:analyze-only | egrep "WARNING|INFO..Building" >> ${START_DIR}/dependencies.txt
-
-echo " ===> ${START_DIR}/dependencies.txt"
+mvn dependency:analyze-only | egrep "WARNING|INFO..Building" > ${START_DIR}/dependencies.txt
--- bin/local_maven_build.sh
+++ bin/local_maven_build.sh
@@ -11,47 +11,5 @@ REPOS_DIR=${START_DIR}/$SCRIPT_HOME/../mrt-services
 export ECR_REGISTRY=it-docker-registry
 export SSM_SKIP_RESOLUTION=Y
 
-echo "Building mrt-core"
 cd $REPOS_DIR
-cd dep_core/mrt-core2
-mvn clean install || exit 1
-
-echo "Building mrt-cloud"
-cd $REPOS_DIR
-cd dep_cloud/mrt-cloud
-mvn clean install || exit 1
-
-echo "Building cdl-zk-queue"
-cd $REPOS_DIR
-cd dep_cdlzk/cdl-zk-queue
-mvn clean install || exit 1
-
-echo "Building mrt-zoo"
-cd $REPOS_DIR
-cd dep_zoo/mrt-zoo
-mvn clean install || exit 1
-
-echo "Building mrt-inventory"
-cd $REPOS_DIR
-cd inventory/mrt-inventory
-mvn clean install || exit 1
-
-echo "Building mrt-replic"
-cd $REPOS_DIR
-cd replic/mrt-replic
-mvn clean install || exit 1
-
-echo "Building mrt-audit"
-cd $REPOS_DIR
-cd audit/mrt-audit
-mvn clean install || exit 1
-
-echo "Building mrt-store"
-cd $REPOS_DIR
-cd store/mrt-store
-mvn clean install || exit 1
-
-echo "Building mrt-ingest"
-cd $REPOS_DIR
-cd ingest/mrt-ingest
-mvn clean install || exit 1
+mvn clean install

3908eef99079c4eabb3b8fc0e7a0814cce664cfb
Terry Brady <terrence.brady@ucop.edu>
2023-03-01 09:26:21 -0800

    update commits

--- mrt-services/zoo/listAccessLarge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#*********************************************************************
-#   Copyright 2019 Regents of the University of California
-#   All rights reserved
-#*********************************************************************
-listQueue.sh /accessLarge.1

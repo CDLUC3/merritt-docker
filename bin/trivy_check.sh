deb1bdc4f28038a8e409eb478682c8d36fff10f4
Terry Brady <terrence.brady@ucop.edu>
2023-06-05 14:33:55 -0700

    update restart and trivy

--- bin/trivy_check.sh
+++ bin/trivy_check.sh
@@ -31,3 +31,44 @@ aws ecr get-login-password --region us-west-2 | \
 ~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-store:dev
 ~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-audit:dev
 ~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-replic:dev
+
+# ruby images
+~/trivy/trivy --scanners vuln image --severity CRITICAL ruby:3
+~/trivy/trivy --scanners vuln image --severity CRITICAL --ignore-unfixed ruby:3
+~/trivy/trivy --scanners vuln image --severity CRITICAL --ignore-unfixed ${ECR_REGISTRY}/callback
+~/trivy/trivy --scanners vuln image --severity CRITICAL --ignore-unfixed ${ECR_REGISTRY}/mock-merritt-it:dev
+~/trivy/trivy --scanners vuln image --severity CRITICAL --ignore-unfixed ${ECR_REGISTRY}/pm-server:dev
+~/trivy/trivy --scanners vuln image --severity CRITICAL --ignore-unfixed ${ECR_REGISTRY}/mrt-dashboard
+~/trivy/trivy --scanners vuln image --severity CRITICAL public.ecr.aws/lambda/ruby:2.7
+~/trivy/trivy --scanners vuln image --severity CRITICAL public.ecr.aws/lambda/ruby:3
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mysql-ruby-lambda
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/uc3-mrt-admin-lambda:dev
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/uc3-mrt-colladmin-lambda:dev
+# no longer used
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/simulate-lambda-alb
+
+
+# note that libarchive-tools adds 2 unfixable vulnerabilities
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-integ-tests
+
+# mysql
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-it-database:dev
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-it-database-audit-replic:dev
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-database
+
+# minio
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-minio-it:dev
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-minio-it-with-content:dev
+
+# other images
+~/trivy/trivy --scanners vuln image --severity CRITICAL zookeeper
+~/trivy/trivy --scanners vuln image --severity CRITICAL ghusta/fakesmtp
+#   ubuntu + curl
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-init
+
+# other images - investigate issues
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/mrt-opendj
+~/trivy/trivy --scanners vuln image --severity CRITICAL opensearchproject/opensearch:1.2.4
+~/trivy/trivy --scanners vuln image --severity CRITICAL opensearchproject/opensearch-dashboards:1.2.0
+~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/logstash-oss:dev
+#~/trivy/trivy --scanners vuln image --severity CRITICAL ${ECR_REGISTRY}/filebeat:dev

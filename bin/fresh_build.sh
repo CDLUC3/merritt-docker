9df56399109a6dc5315f2b6928db703f9e77f3f3
Terry Brady <terrence.brady@ucop.edu>
2023-07-19 16:38:03 -0700

    separate logs

--- bin/fresh_build.sh
+++ bin/fresh_build.sh
@@ -4,39 +4,36 @@
 MD_BRANCH=${1:-main}
 BC_LABEL=${2:-main}
 
-LOGSUM=/tmp/build-summary.${MD_BRANCH}.${BC_LABEL}.txt
-LOGSCAN=/tmp/trivy-scan.${MD_BRANCH}.${BC_LABEL}.txt
-LOGSCANFIXED=/tmp/trivy-scan-fixed.${MD_BRANCH}.${BC_LABEL}.txt
-JOBSTAT=/tmp/jobstat.${MD_BRANCH}.${BC_LABEL}.txt
+WKDIR=/apps/dpr2/merritt-workspace/daily-builds/${MD_BRANCH}.${BC_LABEL}
+rm -rf $WKDIR
+mkdir -p $WKDIR
+cd $WKDIR
+pwd
 
-echo > $LOGSUM
+LOGSUM=${WKDIR}/build-summary.txt
+LOGSCAN=${WKDIR}/trivy-scan.txt
+LOGSCANFIXED=${WKDIR}/trivy-scan-fixed.txt
+LOGDOCKER=${WKDIR}/docker.txt
+LOGMAVEN=${WKDIR}/maven.txt
+JOBSTAT=${WKDIR}/jobstat.txt
+
+echo "Working Dir: ${WKDIR}" > $LOGSUM
+echo " - ${LOGSUM}" >> $LOGSUM
+echo " - ${LOGSCAN}" >> $LOGSUM
+echo " - ${LOGSCANFIXED}" >> $LOGSUM
+echo " - ${LOGDOCKER}" >> $LOGSUM
+echo " - ${LOGMAVEN}" >> $LOGSUM
+echo >> $LOGSUM
+
 echo > $LOGSCAN
-echo "Fixable items below.  See the attached report for the full vulnerability list: \n\n">  $LOGSCANFIXED
+echo > $LOGDOCKER
+echo > $LOGMAVEN
+echo "Fixable items below.  See the attached report for the full vulnerability list: \n\n" > $LOGSCANFIXED
 echo "PASS" > $JOBSTAT
-
-WKDIR=/apps/dpr2/merritt-workspace/builds
 
 echo "Build merritt-docker branch: ${MD_BRANCH} using build label ${BC_LABEL}" >> $LOGSUM
 echo >> $LOGSUM
 date >> $LOGSUM
-
-get_flag() {
-  python3 build-config.py|jq -r ".[\"build-config\"].\"$BC_LABEL\".\"$1\""
-}
-
-FLAG_PUSH=`get_flag push`
-FLAG_SCAN=`get_flag scan-fixable`
-FLAG_SCAN_UNFIXED=`get_flag scan-unfixable`
-FLAG_RUN_MAVEN=`get_flag run-maven`
-
-checkout() {
-  cd $WKDIR/merritt-docker
-  SRCH=".[\"build-config\"].\"$BC_LABEL\".tags.\"$2\""
-  branch=`python3 build-config.py|jq -r $SRCH`
-  cd $WKDIR/merritt-docker/mrt-services/$1
-  git checkout origin/$branch
-  echo "checkout dir: $1, repo: $2, branch: $branch" >> $LOGSUM
-}
 
 # get dir of this script
 START_DIR=$(pwd)
@@ -54,32 +51,14 @@ aws ecr get-login-password --region us-west-2 | \
   docker login --username AWS \
     --password-stdin ${ECR_REGISTRY} || exit 1
 
-rm -rf $WKDIR
-mkdir -p $WKDIR
-cd $WKDIR
-pwd
-
-git clone git@github.com:CDLUC3/merritt-docker.git
-cd merritt-docker
-git submodule update --remote --init
-
-echo "Setup ECS login"
-echo "ECR_REGISTRY: $ECR_REGISTRY"
-aws ecr get-login-password --region us-west-2 | \
-  docker login --username AWS \
-    --password-stdin ${ECR_REGISTRY} || exit 1
-
-checkout 'dep_core/mrt-core2' 'mrt-core'
-checkout 'dep_cloud/mrt-cloud' 'mrt-cloud'
-checkout 'dep_cdlzk/cdl-zk-queue' 'cdl-zk-queue'
-checkout 'dep_zoo/mrt-zoo' 'mrt-zoo'
-checkout 'inventory/mrt-inventory' 'mrt-inventory'
-checkout 'store/mrt-store' 'mrt-store'
-checkout 'ingest/mrt-ingest' 'mrt-ingest'
-checkout 'audit/mrt-audit' 'mrt-audit'
-checkout 'replic/mrt-replic' 'mrt-replic'
-checkout 'ui/mrt-dashboard' 'mrt-dashboard'
-checkout 'mrt-integ-tests' 'mrt-integ-tests'
+checkout() {
+  cd $WKDIR/merritt-docker
+  SRCH=".[\"build-config\"].\"$BC_LABEL\".tags.\"$2\""
+  branch=`python3 build-config.py|jq -r $SRCH`
+  cd $WKDIR/merritt-docker/mrt-services/$1
+  git checkout origin/$branch
+  echo "checkout dir: $1, repo: $2, branch: $branch" >> $LOGSUM
+}
 
 scan_image() {
   if [ "$FLAG_SCAN" == "true" ]
@@ -107,7 +86,7 @@ build_image() {
   sleep 2
   echo >> $LOGSUM
   date >> $LOGSUM
-  docker build --pull --build-arg ECR_REGISTRY=${ECR_REGISTRY} --force-rm $3 -t $1 $2 
+  docker build --pull --build-arg ECR_REGISTRY=${ECR_REGISTRY} --force-rm $3 -t $1 $2 2>&1 >> $LOGDOCKER
   rc=$?
   if [ $rc -ne 0 ]; then echo "FAIL" > $JOBSTAT; fi
   echo "Docker build $1, dir: $2, param: $3; Result: $rc" >> $LOGSUM
@@ -118,7 +97,7 @@ build_image_push() {
   build_image $1 $2 "$3"
   if [ "$FLAG_PUSH" == "true" ]
   then
-    docker push $1 
+    docker push $1 2>&1 >> $LOGDOCKER
     rc=$?
     if [ $rc -ne 0 ]; then echo "FAIL" > $JOBSTAT; fi
     echo "Docker push $1; Result: $rc" >> $LOGSUM
@@ -131,7 +110,7 @@ build_it_image() {
   echo >> $LOGSUM
   date >> $LOGSUM
 
-  docker-compose -f $1 build --pull
+  docker-compose -f $1 build --pull 2>&1 >> $LOGDOCKER
   rc=$?
   if [ $rc -ne 0 ]; then echo "FAIL" > $JOBSTAT; fi 
   echo "Compose Build $2, file: $1; Result: $rc" >> $LOGSUM
@@ -140,7 +119,7 @@ build_it_image() {
 
   if [ "$FLAG_PUSH" == "true" ]
   then
-    docker-compose -f $1 push 
+    docker-compose -f $1 push 2>&1 >> $LOGDOCKER
     rc=$?
     if [ $rc -ne 0 ]; then echo "FAIL" > $JOBSTAT; fi
     echo "Compose Push, file: $1; Result: $rc" >> $LOGSUM
@@ -148,6 +127,44 @@ build_it_image() {
     echo "Image push disabled" >> $LOGSUM
   fi
 }
+
+get_flag() {
+  python3 build-config.py|jq -r ".[\"build-config\"].\"$BC_LABEL\".\"$1\""
+}
+
+echo >> $LOGSUM
+echo "Clone merritt-docker; branch $MD_BRANCH" >> $LOGSUM
+echo "-----" >> $LOGSUM
+
+git clone git@github.com:CDLUC3/merritt-docker.git
+cd merritt-docker
+git checkout $MD_BRANCH
+git submodule update --remote --init
+
+FLAG_PUSH=`get_flag push`
+FLAG_SCAN=`get_flag scan-fixable`
+FLAG_SCAN_UNFIXED=`get_flag scan-unfixable`
+FLAG_RUN_MAVEN=`get_flag run-maven`
+
+echo >> $LOGSUM
+echo "Checking out sumbmodule branches for build label $BC_LABEL" >> $LOGSUM
+echo "-----" >> $LOGSUM
+
+checkout 'dep_core/mrt-core2' 'mrt-core'
+checkout 'dep_cloud/mrt-cloud' 'mrt-cloud'
+checkout 'dep_cdlzk/cdl-zk-queue' 'cdl-zk-queue'
+checkout 'dep_zoo/mrt-zoo' 'mrt-zoo'
+checkout 'inventory/mrt-inventory' 'mrt-inventory'
+checkout 'store/mrt-store' 'mrt-store'
+checkout 'ingest/mrt-ingest' 'mrt-ingest'
+checkout 'audit/mrt-audit' 'mrt-audit'
+checkout 'replic/mrt-replic' 'mrt-replic'
+checkout 'ui/mrt-dashboard' 'mrt-dashboard'
+checkout 'mrt-integ-tests' 'mrt-integ-tests'
+
+echo >> $LOGSUM
+echo "Build Integration Test Docker Images" >> $LOGSUM
+echo "-----" >> $LOGSUM
 
 cd $WKDIR/merritt-docker/mrt-inttest-services
 
@@ -157,20 +174,28 @@ build_it_image mrt-it-database/docker-compose-audit-replic.yml ${ECR_REGISTRY}/m
 build_it_image mrt-minio-it/docker-compose.yml ${ECR_REGISTRY}/mrt-minio-it:dev
 build_it_image mrt-minio-it-with-content/docker-compose.yml ${ECR_REGISTRY}/mrt-minio-it-with-content:dev
 
+echo >> $LOGSUM
+echo "Run maven builds and integration tests" >> $LOGSUM
+echo "-----" >> $LOGSUM
+
 cd ../mrt-services
 
 if [ "$FLAG_RUN_MAVEN" == "true" ]
 then
   echo >> $LOGSUM
   date >> $LOGSUM
-  mvn clean install -Pparent
-  mvn clean install
+  mvn clean install -Pparent 2>&1 >> $LOGMAVEN
+  mvn clean install 2>&1 >> $LOGMAVEN
   rc=$?
-  if [ $rc -ne 0 ]; then STATUS='FAIL'; fi
+  if [ $rc -ne 0 ]; then echo "FAIL" > $JOBSTAT; fi
   echo "Maven Build; Result: $rc" >> $LOGSUM
 else 
   echo "Maven build disabled" >> $LOGSUM
 fi
+
+echo >> $LOGSUM
+echo "Build Merritt Images" >> $LOGSUM
+echo "-----" >> $LOGSUM
 
 build_image_push ${ECR_REGISTRY}/dep-cdlmvn:dev dep_cdlmvn
 build_image_push ${ECR_REGISTRY}/mrt-core2:dev dep_core
@@ -185,10 +210,21 @@ build_image_push ${ECR_REGISTRY}/mrt-audit:dev audit
 build_image_push ${ECR_REGISTRY}/mrt-replic:dev replic
 
 build_image_push ${ECR_REGISTRY}/mrt-dashboard ui
+
+echo >> $LOGSUM
+echo "Build Docker Stack Images" >> $LOGSUM
+echo "-----" >> $LOGSUM
+
 build_image_push ${ECR_REGISTRY}/mrt-database mysql
 build_image_push ${ECR_REGISTRY}/mrt-opendj ldap
 build_image_push ${ECR_REGISTRY}/mrt-init ldap
 build_image_push ${ECR_REGISTRY}/callback callback
+
+build_image ${ECR_REGISTRY}/logstash-oss:dev opensearch/logstash
+
+echo >> $LOGSUM
+echo "Build Merritt Lambda Images" >> $LOGSUM
+echo "-----" >> $LOGSUM
 
 build_image_push ${ECR_REGISTRY}/mysql-ruby-lambda mrt-admin-lambda/mysql-ruby-lambda
 build_image_push ${ECR_REGISTRY}/uc3-mrt-admin-common:dev mrt-admin-lambda/src-common
@@ -197,14 +233,18 @@ build_image_push ${ECR_REGISTRY}/uc3-mrt-colladmin-lambda:dev mrt-admin-lambda/s
 build_image_push ${ECR_REGISTRY}/uc3-mrt-cognitousers:dev mrt-admin-lambda/cognito-lambda-nonvpc
 build_image_push ${ECR_REGISTRY}/simulate-lambda-alb mrt-admin-lambda/simulate-lambda-alb
 
-build_image ${ECR_REGISTRY}/logstash-oss:dev opensearch/logstash
+echo >> $LOGSUM
+echo "Build Merritt End to End Test Images" >> $LOGSUM
+echo "-----" >> $LOGSUM
 
 cd ../mrt-integ-tests
 build_image_push ${ECR_REGISTRY}/mrt-integ-tests .
 build_image standalone-chrome-download-folder chrome-driver
 
 echo >> $LOGSUM
+echo "Scan Supporting Docker Stack Images" >> $LOGSUM
 date >> $LOGSUM
+
 scan_image zookeeper
 scan_image ghusta/fakesmtp
 scan_image opensearchproject/opensearch
@@ -216,4 +256,3 @@ DIST=`get_ssm_value_by_name 'batch/email'`
 STATUS=`cat $JOBSTAT`
 cat $LOGSUM | mail -a $LOGSCAN -a $LOGSCANFIXED -s "${STATUS}: Merritt Daily Build and Docker Image Scan" ${DIST//,/}
 echo "${STATUS}: Merritt Daily Build and Docker Image Scan"
-rm $LOGSUM $LOGSCAN $LOGSCANFIXED

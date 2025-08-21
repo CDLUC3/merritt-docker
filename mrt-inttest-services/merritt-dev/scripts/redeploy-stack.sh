c5356ef685907acfdfd546932a416bda6e20ebf3
Terry Brady <terrywbrady@gmail.com>
2025-08-21 11:09:21 -0700

    enable ecs logging

--- mrt-inttest-services/merritt-dev/scripts/redeploy-stack.sh
+++ mrt-inttest-services/merritt-dev/scripts/redeploy-stack.sh
@@ -6,54 +6,54 @@ if [[ "$MERRITT_ECS" == "ecs-dev" ]]
 then
   export ECS_STACK_NAME=mrt-${MERRITT_ECS}-stack
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service ingest       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service inventory    --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service audit        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service replic       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service store        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service ui           --force-new-deployment --desired-count 2 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service ingest       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service inventory    --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service audit        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service replic       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service store        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service ui           --force-new-deployment --desired-count 2 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services admintool ingest inventory audit replic store ui
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
 
   stack_init
 elif [[ "$MERRITT_ECS" == "ecs-ephemeral" ]]
 then
   export ECS_STACK_NAME=mrt-${MERRITT_ECS}-stack
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service ingest       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service inventory    --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service audit        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service replic       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service store        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service ui           --force-new-deployment --desired-count 2 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service ingest       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service inventory    --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service audit        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service replic       --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service store        --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service ui           --force-new-deployment --desired-count 2 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services admintool ingest inventory audit replic store ui
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
 
   stack_init
 elif [[ "$MERRITT_ECS" == "ecs-dbsnapshot" ]]
 then
   export ECS_STACK_NAME=mrt-${MERRITT_ECS}-stack
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service ui           --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service ui           --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services admintool ui merrittdev
 
   stack_init
 elif [[ "$MERRITT_ECS" == "ecs-stg" ]]
 then
   export ECS_STACK_NAME=mrt-${MERRITT_ECS}-stack
-  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services admintool merrittdev
 elif [[ "$MERRITT_ECS" == "ecs-prd" ]]
 then
   export ECS_STACK_NAME=mrt-${MERRITT_ECS}-stack
-  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager > /dev/null
-  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service admintool    --force-new-deployment --desired-count 2 --output yaml --no-cli-pager 
+  aws ecs update-service --cluster $ECS_STACK_NAME --service merrittdev   --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services admintool merrittdev
 fi
--- mrt-inttest-services/merritt-dev/scripts/redeploy-zk.sh
+++ mrt-inttest-services/merritt-dev/scripts/redeploy-zk.sh
@@ -9,13 +9,13 @@ then
   zk_snapshot
   sleep 30
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo1 --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo1 --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services zoo1
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo2 --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo2 --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services zoo2
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo3 --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo3 --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services zoo3
 elif [[ "$MERRITT_ECS" == "ecs-ephemeral" ]]
 then
@@ -23,7 +23,7 @@ then
   zk_snapshot
   sleep 30
 
-  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo --force-new-deployment --desired-count 1 --output yaml --no-cli-pager > /dev/null
+  aws ecs update-service --cluster $ECS_STACK_NAME --service zoo --force-new-deployment --desired-count 1 --output yaml --no-cli-pager 
   aws ecs wait services-stable --cluster $ECS_STACK_NAME --services zoo
 
   zk_restore

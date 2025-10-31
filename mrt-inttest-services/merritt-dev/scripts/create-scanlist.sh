7ed9ba4ea590d19d1ea69ecc826b17fab176da5c
Terry Brady <terrywbrady@gmail.com>
2025-10-31 16:30:18 -0700

    update scanlist logic

--- mrt-inttest-services/merritt-dev/scripts/create-scanlist.sh
+++ mrt-inttest-services/merritt-dev/scripts/create-scanlist.sh
@@ -5,9 +5,13 @@ case $MERRITT_ECS in
     aws s3 ls --recursive s3://${BUCKET7777}/ | cut -c 32- > /tmp/scanlist
     aws s3 cp /tmp/scanlist s3://${BUCKET8888}/scanlist/7777.log
     ;;
-  "ecs-ephemeral","docker")
+  "ecs-ephemeral")
     aws s3 ls --profile minio-ephemeral --recursive s3://${BUCKET7777}/ | cut -c 32- > /tmp/scanlist
     aws s3 cp --profile minio-ephemeral /tmp/scanlist s3://${BUCKET8888}/scanlist/7777.log
+    ;;
+  "docker")
+    aws s3 ls --profile minio-docker --recursive s3://${BUCKET7777}/ | cut -c 32- > /tmp/scanlist
+    aws s3 cp --profile minio-docker /tmp/scanlist s3://${BUCKET8888}/scanlist/7777.log
     ;;
   *)
     echo "create-scanlist not supported for $MERRITT_ECS"

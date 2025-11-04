b582fd8a3abfe8859e00bcee0ffb254051c15af8
Terry Brady <terrywbrady@gmail.com>
2025-11-04 12:01:10 -0800

    fix set creds script

--- mrt-inttest-services/merritt-dev/scripts/set-credentials.sh
+++ mrt-inttest-services/merritt-dev/scripts/set-credentials.sh
@@ -5,16 +5,16 @@ if [[ -z $SSM_ROOT_PATH ]]; then
   exit 1
 fi
 
-ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-accessKey --query Parameter.Value --output text || echo not_applicable)
-sed -i 's/TEMPLATE_WASABI_ACCESS_KEY_ID/'"$ssmval"'/' /root/.aws/credentials
-ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-secretKey --query Parameter.Value --output text || echo not_applicable)
-sed -i 's/TEMPLATE_WASABI_SECRET_ACCESS_KEY/'"$ssmval"'/' /root/.aws/credentials
-ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-accessKey --query Parameter.Value --output text || echo not_applicable)
-sed -i 's/TEMPLATE_SDSC_ACCESS_KEY_ID/'"$ssmval"'/' /root/.aws/credentials
-ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-secretKey --query Parameter.Value --output text || echo not_applicable)
-sed -i 's/TEMPLATE_SDSC_SECRET_ACCESS_KEY/'"$ssmval"'/' /root/.aws/credentials
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-accessKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_WASABI_ACCESS_KEY_ID|$ssmval|" /root/.aws/credentials
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-secretKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_WASABI_SECRET_ACCESS_KEY|$ssmval|" /root/.aws/credentials
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-accessKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_SDSC_ACCESS_KEY_ID|$ssmval|" /root/.aws/credentials
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-secretKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_SDSC_SECRET_ACCESS_KEY|$ssmval|" /root/.aws/credentials
 
 ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-endpoint --query Parameter.Value --output text || echo not_applicable)
-sed -i 's/TEMPLATE_SDSC_ENDPOINT_URL/'"$ssmval"'/' /root/.aws/config
+sed -i -e "s|TEMPLATE_SDSC_ENDPOINT_URL|$ssmval|" /root/.aws/config
 ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-endpoint --query Parameter.Value --output text || echo not_applicable)
-sed -i 's/TEMPLATE_WASABI_ENDPOINT_URL/'"$ssmval"'/' /root/.aws/config
+sed -i -e "s|TEMPLATE_WASABI_ENDPOINT_URL|$ssmval|" /root/.aws/config

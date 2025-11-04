7d60553358264408f5095370cc7e4e8ef10c9fe4
Terry Brady <terrywbrady@gmail.com>
2025-11-04 11:12:28 -0800

    set creds script

--- mrt-inttest-services/merritt-dev/scripts/set-credentials.sh
+++ mrt-inttest-services/merritt-dev/scripts/set-credentials.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo "Warning: SDSC and Wasabi credentials are not yet integrated into this script"
+if [[ -z $SSM_ROOT_PATH ]]; then
+  echo "Environment variable SSM_ROOT_PATH only defined for DEV environments"
+  exit 1
+fi
 
-ssmval=test_value
-# ssmval=$(aws ssm get-parameter --name /uc3/mrt/ecs/merritt-dev/wasabi/access-key-id --query Parameter.Value --output text)
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-accessKey --query Parameter.Value --output text || echo not_applicable)
 sed -i 's/TEMPLATE_WASABI_ACCESS_KEY_ID/'"$ssmval"'/' /root/.aws/credentials
-# ssmval=$(aws ssm get-parameter --name /uc3/mrt/ecs/merritt-dev/wasabi/secret-access-key --query Parameter.Value --output text)
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-secretKey --query Parameter.Value --output text || echo not_applicable)
 sed -i 's/TEMPLATE_WASABI_SECRET_ACCESS_KEY/'"$ssmval"'/' /root/.aws/credentials
-# ssmval=$(aws ssm get-parameter --name /uc3/mrt/ecs/merritt-dev/sdsc/access-key-id --query Parameter.Value --output text)
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-accessKey --query Parameter.Value --output text || echo not_applicable)
 sed -i 's/TEMPLATE_SDSC_ACCESS_KEY_ID/'"$ssmval"'/' /root/.aws/credentials
-# ssmval=$(aws ssm get-parameter --name /uc3/mrt/ecs/merritt-dev/sdsc/secret-access-key --query Parameter.Value --output text)
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-secretKey --query Parameter.Value --output text || echo not_applicable)
 sed -i 's/TEMPLATE_SDSC_SECRET_ACCESS_KEY/'"$ssmval"'/' /root/.aws/credentials
 
-# ssmval=$(aws ssm get-parameter --name /uc3/mrt/ecs/merritt-dev/sdsc/endpoint-url --query Parameter.Value --output text)
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-endpoint --query Parameter.Value --output text || echo not_applicable)
 sed -i 's/TEMPLATE_SDSC_ENDPOINT_URL/'"$ssmval"'/' /root/.aws/config
-# ssmval=$(aws ssm get-parameter --name /uc3/mrt/ecs/merritt-dev/wasabi/endpoint-url --query Parameter.Value --output text)
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-endpoint --query Parameter.Value --output text || echo not_applicable)
 sed -i 's/TEMPLATE_WASABI_ENDPOINT_URL/'"$ssmval"'/' /root/.aws/config

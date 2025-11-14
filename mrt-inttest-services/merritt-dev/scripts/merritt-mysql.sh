8ad44ce23d46274c765477b47e4000b1657f22f0
Terry Brady <terrywbrady@gmail.com>
2025-11-14 13:14:06 -0800

    db connection

--- mrt-inttest-services/merritt-dev/scripts/merritt-mysql.sh
+++ mrt-inttest-services/merritt-dev/scripts/merritt-mysql.sh
@@ -17,14 +17,14 @@ then
   DBPASS=$(aws ssm get-parameter --name /uc3/mrt/dev/billing/readwrite/db-password --query Parameter.Value --with-decryption --output text)
 elif [[ "$MERRITT_ECS" == "ecs-stg" ]]
 then
-  DBHOST=$(aws ssm get-parameter --name /uc3/mrt/stg/billing/db-host --query Parameter.Value --output text)
-  DBUSER=$(aws ssm get-parameter --name /uc3/mrt/stg/billing/readwrite/db-user --query Parameter.Value --output text)
-  DBPASS=$(aws ssm get-parameter --name /uc3/mrt/stg/billing/readwrite/db-password --query Parameter.Value --with-decryption --output text)
+  DBHOST=$(aws ssm get-parameter --name /uc3/mrt/stg/inv/db-host --query Parameter.Value --output text)
+  DBUSER=$(aws ssm get-parameter --name /uc3/mrt/stg/inv/readwrite/db-user --query Parameter.Value --output text)
+  DBPASS=$(aws ssm get-parameter --name /uc3/mrt/stg/inv/readwrite/db-password --query Parameter.Value --with-decryption --output text)
 elif [[ "$MERRITT_ECS" == "ecs-prd" ]]
 then
-  DBHOST=$(aws ssm get-parameter --name /uc3/mrt/prd/billing/db-host --query Parameter.Value --output text)
-  DBUSER=$(aws ssm get-parameter --name /uc3/mrt/prd/billing/readwrite/db-user --query Parameter.Value --output text)
-  DBPASS=$(aws ssm get-parameter --name /uc3/mrt/prd/billing/readwrite/db-password --query Parameter.Value --with-decryption --output text)
+  DBHOST=$(aws ssm get-parameter --name /uc3/mrt/prd/inv/db-host --query Parameter.Value --output text)
+  DBUSER=$(aws ssm get-parameter --name /uc3/mrt/prd/inv/readwrite/db-user --query Parameter.Value --output text)
+  DBPASS=$(aws ssm get-parameter --name /uc3/mrt/prd/inv/readwrite/db-password --query Parameter.Value --with-decryption --output text)
 fi
 
 mysql -h $DBHOST -u $DBUSER --password=$DBPASS --database=$DBDATABASE "$@"

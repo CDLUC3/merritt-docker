238a372ae9584732cb80477b859c1cee2a16c2a1
Terry Brady <terrywbrady@gmail.com>
2025-11-18 09:19:25 -0800

    refine error creation script

--- mrt-inttest-services/merritt-dev/scripts/create-audit-error.sh
+++ mrt-inttest-services/merritt-dev/scripts/create-audit-error.sh
@@ -5,13 +5,19 @@ if [[ -z $BUCKET8888 ]]; then
   exit 1
 fi
 
+if [[ "$MERRITT_ECS" == "docker" || "$MERRITT_ECS" == "ecs-ephemeral" ]]
+then
+  export AWS_PROFILE=minio
+  export AWS_REGION=us-east-1
+fi
+
 arkp1=ark:/99999/
-arkp2=$(/merritt-s3.sh ls s3://$BUCKET8888/$arkp1 | cut -c32- | grep system | tail -1)
+arkp2=$(aws s3 ls s3://$BUCKET8888/$arkp1 | cut -c32- | grep system | tail -1)
 
-/merritt-s3.sh cp s3://$BUCKET8888/${arkp1}${arkp2}mrt-mom.txt /tmp/mrt-mom.txt
+aws s3 cp s3://$BUCKET8888/${arkp1}${arkp2}mrt-mom.txt /tmp/mrt-mom.txt
 echo 'error' >> /tmp/mrt-mom.txt
-/merritt-s3.sh cp /tmp/mrt-mom.txt s3://$BUCKET8888/${arkp1}${arkp2}
+aws s3 cp /tmp/mrt-mom.txt s3://$BUCKET8888/${arkp1}${arkp2}
 
-/merritt-s3.sh cp s3://$BUCKET8888/${arkp1}${arkp2}mrt-ingest.txt /tmp/mrt-ingest.txt
+aws s3 cp s3://$BUCKET8888/${arkp1}${arkp2}mrt-ingest.txt /tmp/mrt-ingest.txt
 sed -i -e 's/e/a/g' /tmp/mrt-ingest.txt
-/merritt-s3.sh cp /tmp/mrt-ingest.txt s3://$BUCKET8888/${arkp1}${arkp2}
+aws s3 cp /tmp/mrt-ingest.txt s3://$BUCKET8888/${arkp1}${arkp2}
--- mrt-inttest-services/merritt-dev/scripts/create-scan-error.sh
+++ mrt-inttest-services/merritt-dev/scripts/create-scan-error.sh
@@ -5,11 +5,17 @@ if [[ -z $BUCKET7777 ]]; then
   exit 1
 fi
 
+if [[ "$MERRITT_ECS" == "docker" || "$MERRITT_ECS" == "ecs-ephemeral" ]]
+then
+  export AWS_PROFILE=minio
+  export AWS_REGION=us-east-1
+fi
+
 echo 'hello' > /tmp/test.txt
 
-/merritt-s3.sh cp /tmp/test.txt s3://$BUCKET7777/scan-error.txt
-/merritt-s3.sh cp /tmp/test.txt s3://$BUCKET7777/ark:/00000/fk00000000/scan-error.txt
+aws s3 cp /tmp/test.txt s3://$BUCKET7777/scan-error.txt
+aws s3 cp /tmp/test.txt s3://$BUCKET7777/ark:/00000/fk00000000/scan-error.txt
 
 arkp1=ark:/99999/
-arkp2=$(/merritt-s3.sh ls s3://$BUCKET7777/$arkp1 | cut -c32- | grep producer | tail -1)
-/merritt-s3.sh cp /tmp/test.txt s3://$BUCKET7777/${arkp1}${arkp2}scan-error.txt
+arkp2=$(aws s3 ls s3://$BUCKET7777/$arkp1 | cut -c32- | grep producer | tail -1)
+aws s3 cp /tmp/test.txt s3://$BUCKET7777/${arkp1}${arkp2}scan-error.txt

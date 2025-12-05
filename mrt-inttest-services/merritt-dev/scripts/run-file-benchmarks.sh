6cf73a63244ff96bb3c05db223cf61bc87cd983c
Terry Brady <terrywbrady@gmail.com>
2025-12-05 10:57:48 -0800

    add dev logic

--- mrt-inttest-services/merritt-dev/scripts/run-file-benchmarks.sh
+++ mrt-inttest-services/merritt-dev/scripts/run-file-benchmarks.sh
@@ -46,6 +46,9 @@ if [[ "$MERRITT_ECS" == "ecs-prd" ]]
 then
   run_tests Audit_benchmark_20mb 20_mb_random.dat '5001 9501 2001'
   run_tests Audit_benchmark_100mb 100_mb_random.dat '5001 9501 2001'
+elif [[ "$MERRITT_ECS" == "ecs-dev" ]]
+then
+  run_tests 2025_10_20_0834_combo README.md '7777 8888'
 fi
 
 grep -qv "ERROR|FAIL" $statfile || task_fail

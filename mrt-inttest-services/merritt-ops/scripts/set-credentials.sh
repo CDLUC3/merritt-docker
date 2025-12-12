36aca13c6a9bbbfb8ba663968818e9f3afc77eaa
Terry Brady <terrywbrady@gmail.com>
2025-12-12 15:04:08 -0800

    set creds for merritt-ops

--- mrt-inttest-services/merritt-ops/scripts/set-credentials.sh
+++ mrt-inttest-services/merritt-ops/scripts/set-credentials.sh
@@ -9,12 +9,20 @@ ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-accessK
 sed -i -e "s|TEMPLATE_WASABI_ACCESS_KEY_ID|$ssmval|" /root/.aws/credentials
 ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-secretKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
 sed -i -e "s|TEMPLATE_WASABI_SECRET_ACCESS_KEY|$ssmval|" /root/.aws/credentials
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-endpoint --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_WASABI_ENDPOINT_URL|$ssmval|" /root/.aws/config
+
 ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-accessKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
 sed -i -e "s|TEMPLATE_SDSC_ACCESS_KEY_ID|$ssmval|" /root/.aws/credentials
 ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-secretKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
 sed -i -e "s|TEMPLATE_SDSC_SECRET_ACCESS_KEY|$ssmval|" /root/.aws/credentials
-
 ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-endpoint --query Parameter.Value --output text || echo not_applicable)
 sed -i -e "s|TEMPLATE_SDSC_ENDPOINT_URL|$ssmval|" /root/.aws/config
-ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/wasabi-endpoint --query Parameter.Value --output text || echo not_applicable)
-sed -i -e "s|TEMPLATE_WASABI_ENDPOINT_URL|$ssmval|" /root/.aws/config
+
+# Note that these credential only exist in stage and prod... no dev keys exist
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-s3-accessKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_SDSC_S3_ACCESS_KEY_ID|$ssmval|" /root/.aws/credentials
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-s3-secretKey --with-decryption --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_SDSC_S3_SECRET_ACCESS_KEY|$ssmval|" /root/.aws/credentials
+ssmval=$(aws ssm get-parameter --name ${SSM_ROOT_PATH}cloud/nodes/sdsc-s3-endpoint --query Parameter.Value --output text || echo not_applicable)
+sed -i -e "s|TEMPLATE_SDSC_S3_ENDPOINT_URL|$ssmval|" /root/.aws/config

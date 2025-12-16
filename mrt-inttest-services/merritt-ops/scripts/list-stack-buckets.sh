e579a1af38cf920070a2669768db48de2ee18d81
Terry Brady <terrywbrady@gmail.com>
2025-12-16 11:59:41 -0800

    list buckets

--- mrt-inttest-services/merritt-ops/scripts/list-stack-buckets.sh
+++ mrt-inttest-services/merritt-ops/scripts/list-stack-buckets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s http://admintool:9292/ops/storage/storage-config?admintoolformat=json|jq ".table[].bucket"
+curl -s http://admintool:9292/ops/storage/storage-config?admintoolformat=json | jq -r ".table[][].command"

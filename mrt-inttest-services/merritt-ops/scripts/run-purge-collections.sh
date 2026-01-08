aaba7c2a6033ab3fae2134d3e506aebc3b05e456
Terry Brady <terrywbrady@gmail.com>
2026-01-07 17:10:25 -0800

    run purge

---
+++ mrt-inttest-services/merritt-ops/scripts/run-purge-collections.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+source ./ecs-helpers.sh
+
+export label="Purge Collections"
+export statfile="/tmp/purge-collections-log.txt"
+
+task_init
+
+run_purge() {
+  coll=$1
+  curl -X POST -H "Accept: application/json" -o /tmp/curl.json -s" $(admintool_base)/test/purge/${coll} || return
+  jq '.[]' /tmp/curl.json
+  count=$(jq -r '.|length' /tmp/curl.json)
+  if [ $count -gt 0 ]
+  then
+    run_purge $coll
+  fi
+}
+
+if [[ "$MERRITT_ECS" != "ecs-prd" ]]
+then
+  for coll in merritt_demo merritt_benchmark cdl_wasabi
+  do
+    echo "Purging $coll"
+    echo ""
+    run_purge $coll
+  done
+end
+
+task_complete

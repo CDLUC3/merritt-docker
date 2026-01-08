2d42fd5861f62c668ab609933ea13f92f3be30cc
Terry Brady <terrywbrady@gmail.com>
2026-01-08 09:52:27 -0800

    refine output

--- mrt-inttest-services/merritt-ops/scripts/run-purge-collections.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-purge-collections.sh
@@ -13,7 +13,7 @@ run_purge() {
   echo ""
 
   curl -X POST -H "Accept: application/json" -o /tmp/curl.json -s "$(admintool_base)/test/purge/${coll}" || return
-  jq '.[]' /tmp/curl.json
+  jq '.[].message' /tmp/curl.json
   count=$(jq -r '.|length' /tmp/curl.json)
   if [ $count -gt 0 ]
   then

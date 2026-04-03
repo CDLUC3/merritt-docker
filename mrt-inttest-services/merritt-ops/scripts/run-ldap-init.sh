c32a361bfa1be78d86937dcce86fb83996b21aa6
Terry Brady <terrywbrady@gmail.com>
2026-04-03 13:51:08 -0700

    ldap init

---
+++ mrt-inttest-services/merritt-ops/scripts/run-ldap-init.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+source ./ecs-helpers.sh
+
+export label="Run LDAP Init"
+export statfile="/tmp/run-ldap-init-log.txt"
+
+task_init
+
+rm -rf /merritt-filesys/ldap
+mkdir -p /merritt-filesys/ldap/config /merritt-filesys/ldap/db
+chmod 666 /merritt-filesys/ldap/config /merritt-filesys/ldap/db
+
+PRDBUCK=$(aws ssm get-parameter --name /uc3/mrt/config-bucket-prd --query Parameter.Value --output text)
+
+if [[ "$MERRITT_ECS" == "ecs-stg" ]]
+then
+  aws s3 cp s3://$PRDBUCK/uc3/mrt/ldap/stg/backup/latest.ldif /merritt-filesys/ldap/barebones.ldif
+elif [[ "$MERRITT_ECS" == "ecs-prd" ]]
+then
+  aws s3 cp s3://$PRDBUCK/uc3/mrt/ldap/prd/backup/latest.ldif /merritt-filesys/ldap/barebones.ldif
+fi
+
+task_complete

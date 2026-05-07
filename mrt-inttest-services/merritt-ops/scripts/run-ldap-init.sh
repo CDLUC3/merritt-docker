80fef5a7fcf38e4e7b0acb2d87f8a329e5bb34b3
Terry Brady <terrywbrady@gmail.com>
2026-05-07 16:22:02 -0700

    ldap init script

--- mrt-inttest-services/merritt-ops/scripts/run-ldap-init.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-ldap-init.sh
@@ -5,18 +5,25 @@ source ./ecs-helpers.sh
 export label="Run LDAP Init"
 export statfile="/tmp/run-ldap-init-log.txt"
 
+ldap_init() {
+  echo "Initializing LDAP data for ECS environment: $MERRITT_ECS"
+  rm -rf /merritt-filesys/ldap
+  mkdir -p /merritt-filesys/ldap/config /merritt-filesys/ldap/db /merritt-filesys/ldap/import
+  chmod 777 /merritt-filesys/ldap/config /merritt-filesys/ldap/db /merritt-filesys/ldap/import
+
+  find /merritt-filesys/ldap -print
+
+  if [[ "$MERRITT_ECS" == "ecs-stg" ]]
+  then
+    aws s3 cp s3://${S3CONFIG_BUCKET}/uc3/mrt/ldap/stg/backup/latest.ldif /merritt-filesys/ldap/import/import.ldif
+  elif [[ "$MERRITT_ECS" == "ecs-prd" ]]
+  then
+    aws s3 cp s3://${S3CONFIG_BUCKET}/uc3/mrt/ldap/prd/backup/latest.ldif /merritt-filesys/ldap/import/import.ldif
+  fi
+}
+
 task_init
 
-rm -rf /merritt-filesys/ldap
-mkdir -p /merritt-filesys/ldap/config /merritt-filesys/ldap/db /merritt-filesys/ldap/import
-chmod 777 /merritt-filesys/ldap/config /merritt-filesys/ldap/db /merritt-filesys/ldap/import
-
-if [[ "$MERRITT_ECS" == "ecs-stg" ]]
-then
-  aws s3 cp s3://${S3CONFIG_BUCKET}/uc3/mrt/ldap/stg/backup/latest.ldif /merritt-filesys/ldap/import/import.ldif
-elif [[ "$MERRITT_ECS" == "ecs-prd" ]]
-then
-  aws s3 cp s3://${S3CONFIG_BUCKET}/uc3/mrt/ldap/prd/backup/latest.ldif /merritt-filesys/ldap/import/import.ldif
-fi
+ldap_init
 
 task_complete

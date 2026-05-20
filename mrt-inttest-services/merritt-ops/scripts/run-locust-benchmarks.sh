21565c60022bfe120d533ad1758c493a4ac56f09
Terry Brady <terrywbrady@gmail.com>
2026-05-20 11:01:20 -0700

    save test output to s3

--- mrt-inttest-services/merritt-ops/scripts/run-locust-benchmarks.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-locust-benchmarks.sh
@@ -9,7 +9,23 @@ task_init
 
 # Return Code ignores tee 
 set -o pipefail
-/mrt-locust/run_locust.sh | tee -a $statfile || task_fail
+/mrt-locust/run_locust.sh | tee -a $statfile || FAIL=1
 set +o pipefail
 
-task_complete
+rptfile="$(date +%Y%m%d-%H%M%S).txt"
+aws s3 cp $statfile "s3://${S3REPORT_BUCKET}/locust/${rptfile}"
+
+mv $statfile $statfile.tmp
+
+echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile
+echo "" >> $statfile
+echo "${baseurl}ops/s3-reports/retrieve?report=locust%2F${rptfile}" >> $statfile
+echo "" >> $statfile
+cat $statfile.tmp >> $statfile
+
+if [ $FAIL -eq 1 ]
+then
+  task_fail
+else
+  task_complete
+fi
--- mrt-inttest-services/merritt-ops/scripts/run-unit-test-endpoints.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-unit-test-endpoints.sh
@@ -8,7 +8,23 @@ export statfile="/tmp/admin-unit-log.txt"
 task_init
 
 set -o pipefail
-admintool_test_routes | tee -a $statfile || task_fail
+admintool_test_routes | tee -a $statfile || FAIL=1
 set +o pipefail
 
-task_complete
+rptfile="$(date +%Y%m%d-%H%M%S).txt"
+aws s3 cp $statfile "s3://${S3REPORT_BUCKET}/unit-tests/${rptfile}"
+
+mv $statfile $statfile.tmp
+
+echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile
+echo "" >> $statfile
+echo "${baseurl}ops/s3-reports/retrieve?report=unit-tests%2F${rptfile}" >> $statfile
+echo "" >> $statfile
+cat $statfile.tmp >> $statfile
+
+if [ $FAIL -eq 1 ]
+then
+  task_fail
+else
+  task_complete
+fi

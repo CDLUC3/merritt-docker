8b8850c26cf3a5918dee4fd07bf27ed1be754968
Terry Brady <terrywbrady@gmail.com>
2026-05-20 18:11:22 -0700

    markdown messages

--- mrt-inttest-services/merritt-ops/scripts/run-unit-test-endpoints.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-unit-test-endpoints.sh
@@ -15,20 +15,18 @@ set +o pipefail
 rptfile="$(date +%Y%m%d-%H%M%S).txt"
 aws s3 cp $statfile "s3://${S3REPORT_BUCKET}/unit-tests/${rptfile}"
 
-mv $statfile $statfile.tmp
-
-echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile
-echo "" >> $statfile
-echo "${baseurl}ops/s3-reports/unit-test-results?report=unit-tests%2F${rptfile}" >> $statfile
-echo "" >> $statfile
-cat $statfile.tmp >> $statfile
+echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile.slack
+echo "" >> $statfile.slack
+echo "${baseurl}ops/s3-reports/unit-test-results?report=unit-tests%2F${rptfile}" >> $statfile.slack
+echo "" >> $statfile.slack
 
 export SLACK_BOT_TOKEN=$(aws ssm get-parameter --name "${SLACK_BOT_SSM}" --with-decryption --query "Parameter.Value" --output text)
 
 if [ $FAIL -eq 1 ]
 then
   task_fail N
-  ruby slack_message.rb $statfile
+  ruby slack_message.rb $statfile.slack
 else
   task_complete
+  ruby slack_message.rb $statfile.slack
 fi

d8040d53c7ebe85f6e558128ba39d09870ec1f93
Terry Brady <terrywbrady@gmail.com>
2026-06-05 15:31:57 -0700

    efs init script

---
+++ mrt-inttest-services/merritt-ops/scripts/efs-init.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+for dir in /ingest/queue /uploads /assemblies /zk-snapshots /ldap /ldap/import /ldapreplica
+do
+  mkdir -p /merritt-filesys/$dir
+  chmod 775 /merritt-filesys/$dir
+done
+
+for dir in /ldap /ldap/import /ldapreplica
+do
+  chown opendj:opendj /merritt-filesys/$dir
+done

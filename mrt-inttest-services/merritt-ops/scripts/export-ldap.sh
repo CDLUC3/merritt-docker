c04c806484db2fb560a1efd286ed2c90ad85a0e9
Terry Brady <terrywbrady@gmail.com>
2026-06-05 15:59:02 -0700

    ldap export script

--- mrt-inttest-services/merritt-ops/scripts/export-ldap.sh
+++ mrt-inttest-services/merritt-ops/scripts/export-ldap.sh
@@ -10,7 +10,7 @@ task_init
 datetime=$(TZ="America/Los_Angeles" date "+%Y-%m-%d_%H:%M")
 filename="${1:-export.${datetime}.ldif}"
 
-ldap=$(aws ecs list-tasks --cluster mrt-ecs-dev-stack --service-name ldap --query taskArns --output text)
+ldap=$(aws ecs list-tasks --cluster $ECS_STACK_NAME --service-name ldap --query taskArns --output text)
 
 aws ecs execute-command --cluster $ECS_STACK_NAME --task $ldap \
   --container ldap --command "/opt/opendj/merritt-export.sh" --interactive

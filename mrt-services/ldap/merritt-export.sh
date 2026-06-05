4e5b5f423a81741c137e5e0651b3b46a6eaf2f20
Terry Brady <terrywbrady@gmail.com>
2026-06-05 14:10:21 -0700

    export handling for ldap

--- mrt-services/ldap/merritt-export.sh
+++ mrt-services/ldap/merritt-export.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-bin/export-ldif --backendID userRoot --bindPassword $ROOT_PASSWORD --ldifFile export.ldif
+datetime=$(TZ="America/Los_Angeles" date "+%Y-%m-%d_%H:%M")
+filename="${1:-export.${datetime}.ldif}"
+
+if [[ "$REPLICA" == "true" ]]
+then
+  echo "is REPLICA, skipping export"
+else
+  datetime=$(TZ="America/Los_Angeles" date "+%Y-%m-%d_%H:%M:%S")
+  bin/export-ldif --backendID userRoot --bindPassword $ROOT_PASSWORD --ldifFile /opt/import/${filename}
+fi

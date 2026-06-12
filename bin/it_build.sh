eef90753c4697096c4bb1dff60fd8d68f7514f70
Terry Brady <terrywbrady@gmail.com>
2026-06-12 09:54:45 -0700

    eliminate minio references

--- bin/it_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker-compose \
-  -f mrt-inttest-services/merritt-maven/docker-compose.yml \
-  -f mrt-inttest-services/merritt-tomcat/docker-compose.yml \
-  -f mrt-inttest-services/mock-merritt-it/docker-compose.yml \
-  -f mrt-inttest-services/mrt-it-database/docker-compose.yml \
-  -f mrt-inttest-services/mrt-it-database/docker-compose-audit-replic.yml \
-  -f mrt-inttest-services/mrt-minio-it/docker-compose.yml \
-  -f mrt-inttest-services/mrt-minio-it-with-content/docker-compose.yml build

fd75b177a83556ee1ea804a9fdf4cfc3d7979ef2
Terry Brady <terrywbrady@gmail.com>
2026-06-16 15:08:59 -0700

    ldap init refine

--- mrt-services/ldap/merritt-replication-init.sh
+++ mrt-services/ldap/merritt-replication-init.sh
@@ -12,10 +12,10 @@ fi
 bin/dsreplication enable \
   --baseDN "$BASE_DN" \
   --host1 $PRIMARY --port1 4444 \
-  --bindDN1 "cn=Directory Manager" --bindPassword1 "$ROOT_PASSWORD" \
+  --bindDN1 "$ROOT_USER_DN" --bindPassword1 "$ROOT_PASSWORD" \
   --replicationPort1 8989 \
   --host2 $SECONDARY --port2 4444 \
-  --bindDN2 "cn=Directory Manager" --bindPassword2 "$ROOT_PASSWORD" \
+  --bindDN2 "$ROOT_USER_DN" --bindPassword2 "$ROOT_PASSWORD" \
   --replicationPort2 8989 \
   --adminUID admin --adminPassword "$ROOT_PASSWORD" \
   --baseDN "$BASE_DN" -X -n

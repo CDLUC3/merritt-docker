1ba3ddec9cfa396605339fb5d029d831214902e4
Terry Brady <terrywbrady@gmail.com>
2026-07-23 11:59:05 -0700

    mail log cleanup

--- mrt-inttest-services/mrt-smtp/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Ensure mail log exists and stream it to container stdout.
-mkdir -p /var/log
-touch /var/log/maillog
-tail -n0 -F /var/log/maillog &
-
-exec /usr/sbin/postfix -v start-fg

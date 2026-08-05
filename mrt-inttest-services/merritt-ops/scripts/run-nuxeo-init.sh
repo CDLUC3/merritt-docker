552a062e4b5773740b32ddc111b658782dd97aa2
Terry Brady <terrywbrady@gmail.com>
2026-08-05 16:30:29 -0700

    nuxeo process update

--- mrt-inttest-services/merritt-ops/scripts/run-nuxeo-init.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-nuxeo-init.sh
@@ -7,6 +7,21 @@ export statfile="/tmp/nuxeo-init-log.txt"
 task_init
 
 NUXEO_DIR="/merritt-filesys/nuxeo/mrt-atom"
+
+if [[ -d "${NUXEO_DIR}" ]]
+then
+  cd ${NUXEO_DIR}
+  git fetch
+  git pull
+else
+  cd /merritt-filesys/nuxeo
+  git clone https://github.com/CDLUC3/mrt-atom.git
+  cd ${NUXEO_DIR}
+fi
+
+bundle install
+mkdir -p LockFile LastUpdate logs
+
 ${NUXEO_DIR}/bin/nuxeoInit.sh || task_fail
 
 task_complete

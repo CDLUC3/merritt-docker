7b2a80ba73fd281d6b72040bc32f6844d9e77525
Terry Brady <terrywbrady@gmail.com>
2026-08-07 15:04:15 -0700

    refine the purge script

--- mrt-inttest-services/merritt-ops/scripts/run-purge-collections.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-purge-collections.sh
@@ -9,10 +9,12 @@ task_init
 
 run_purge() {
   coll=$1
+  days=$2
   echo "Purging $coll" 
   echo ""
 
-  curl -X POST -H "Accept: application/json" -o /tmp/curl.json -s "$(admintool_base)/test/purge/${coll}" || return
+  curl -X POST -H "Accept: application/json" -o /tmp/curl.json -s \
+    "$(admintool_base)/test/purge/${coll}?days=${days}" || return
   jq '.[].message' /tmp/curl.json
   count=$(jq -r '.|length' /tmp/curl.json)
 
@@ -20,7 +22,7 @@ run_purge() {
   then
     if [ $count -gt 0 ]
     then
-      run_purge $coll
+      run_purge $coll $days
     fi
   fi
 }
@@ -29,7 +31,13 @@ if [[ "$MERRITT_ECS" != "ecs-prd" ]]
 then
   for coll in merritt_demo merritt_benchmark cdl_wasabi
   do
-    run_purge $coll | tee -a $statfile
+    run_purge $coll 3 | tee -a $statfile
+  done
+
+  for coll in ucr_lib_nuxeo ucm_lib_dsc ucm_lib_jaac_newsletters \
+    ucm_lib_mcdaniel ucm_lib_acm ucm_lib_mclean ucm_lib_clark
+  do
+    run_purge $coll 0 | tee -a $statfile
   done
 fi
 

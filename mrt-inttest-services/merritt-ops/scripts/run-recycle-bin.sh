cc44140605cfd7e81afcd64a4d74a38e9021044a
Terry Brady <terrywbrady@gmail.com>
2026-08-19 14:40:46 -0700

    set recycle bin script

--- mrt-inttest-services/merritt-ops/scripts/run-recycle-bin.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-recycle-bin.sh
@@ -7,7 +7,7 @@ export statfile="/tmp/recycle-bin-log.txt"
 
 task_init
 
-find /s3files/queue/RecycleBin -maxdepth 1  -name "jid-*" -mtime +7 | xargs rm -rf
-find /s3files/queue/RecycleBin -maxdepth 1  -name "jid-*" -mtime +3h | xargs rm -rf
+find /s3filesys/queue/RecycleBin -maxdepth 1  -name "jid-*" -mtime +7 | xargs rm -rf
+# find /s3filesys/queue/RecycleBin -maxdepth 1  -name "jid-*" -mmin +180 | xargs rm -rf
 
 task_complete

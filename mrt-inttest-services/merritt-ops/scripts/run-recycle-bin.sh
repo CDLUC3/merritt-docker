bfe39bb4d66b5678134b16ab90fd37d71dc2abcf
Terry Brady <terrywbrady@gmail.com>
2026-08-19 16:42:34 -0700

    update recycle bin delete

--- mrt-inttest-services/merritt-ops/scripts/run-recycle-bin.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-recycle-bin.sh
@@ -7,7 +7,7 @@ export statfile="/tmp/recycle-bin-log.txt"
 
 task_init
 
-find /s3filesys/queue/RecycleBin -maxdepth 1  -name "jid-*" -mtime +7 | xargs rm -rf
-# find /s3filesys/queue/RecycleBin -maxdepth 1  -name "jid-*" -mmin +180 | xargs rm -rf
+find /merritt-filesys/ingest/queue/RecycleBin -maxdepth 1  -name "jid-*" -mtime +7 | xargs rm -rf
+# find /merritt-filesys/ingest/queue/RecycleBin -maxdepth 1  -name "jid-*" -mmin +180 | xargs rm -rf
 
 task_complete

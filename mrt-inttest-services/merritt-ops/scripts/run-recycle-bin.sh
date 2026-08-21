8a842d69e23a0f50d8b4a24e88174bc095bcd3c5
Terry Brady <terrywbrady@gmail.com>
2026-08-21 14:48:22 -0700

    Update recycle bin logic

--- mrt-inttest-services/merritt-ops/scripts/run-recycle-bin.sh
+++ mrt-inttest-services/merritt-ops/scripts/run-recycle-bin.sh
@@ -7,7 +7,20 @@ export statfile="/tmp/recycle-bin-log.txt"
 
 task_init
 
+# Replace -mtime X with -mmin X to use minutes instead of days.
+
+# Find recycle bin folders older than 7 days
 find /merritt-filesys/ingest/queue/RecycleBin -maxdepth 1  -name "jid-*" -mtime +7 | xargs rm -rf
-# find /merritt-filesys/ingest/queue/RecycleBin -maxdepth 1  -name "jid-*" -mmin +180 | xargs rm -rf
+
+# Find batch folders older than 30 days that do not still contain a producer directory
+# The checkm files within these folders are occasionally used for troubleshooting.
+oldbids=$(find /merritt-filesys/ingest/queue -maxdepth 1 -name "bid-*" -mtime +30)
+for f in $oldbids
+do
+  # compgen is a bash utility that can evaluate wildcards
+  compgen -G "$f/jid-*/producer" > /dev/null || continue
+  echo "Removing old batch folder: $f"
+  rm -rf $f
+done
 
 task_complete
